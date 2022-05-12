9baad77c5bdd5c5e20d7803115c78d04ffab7fdc
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-05-12 21:34:21 +0300

    App was completed.

--- app/src/main/java/com/metehanbolat/overlappingpanelscompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/overlappingpanelscompose/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.metehanbolat.overlappingpanelscompose.ui.theme.OverlappingPanelsComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,4 +26,90 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun ComposeOverlappingPanels() {
+    val panelState = rememberOverlappingPanelsState()
+    val coroutineScope = rememberCoroutineScope()
+    val gesturesEnabled by remember { mutableStateOf(true) }
+
+    OverlappingPanels(
+        modifier = Modifier.fillMaxSize(),
+        panelsState = panelState,
+        gesturesEnabled = gesturesEnabled,
+        panelStart = {
+            PanelSurface {
+                PanelColumn {
+                    PanelHeaderText(
+                        text = stringResource(id = R.string.app_name),
+                        modifier = Modifier.align(CenterHorizontally)
+                    )
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+                                panelState.closePanels()
+                            }
+                        }
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.app_name)
+                        )
+                    }
+                }
+            }
+        },
+        panelCenter = {
+            PanelSurface {
+                PanelColumn {
+                    PanelHeaderText(
+                        text = stringResource(id = R.string.app_name),
+                        modifier = Modifier.align(CenterHorizontally)
+                    )
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+                                panelState.openStartPanel()
+                            }
+                        }
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.app_name)
+                        )
+                    }
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+                                panelState.openEndPanel()
+                            }
+                        }
+                    ) {
+                        Text(text = stringResource(R.string.app_name))
+                    }
+                }
+            }
+        },
+        panelEnd = {
+            PanelSurface {
+                PanelColumn {
+                    PanelHeaderText(
+                        text = stringResource(id = R.string.app_name),
+                        modifier = Modifier.align(CenterHorizontally)
+                    )
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+                                panelState.closePanels()
+                            }
+                        }
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.app_name)
+                        )
+                    }
+                }
+            }
+        }
+    )
 }
---
+++ app/src/main/java/com/metehanbolat/overlappingpanelscompose/PanelSurface.kt
@@ -0,0 +1,19 @@
+package com.metehanbolat.overlappingpanelscompose
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PanelSurface(
+    content: @Composable () -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+        color = MaterialTheme.colors.background,
+        content = content
+    )
+}
